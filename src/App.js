428f3feab546643866ebc2da5c4a1170234c5d9d
Paul Tournemaine <p.tournemaine@a2si-identit.fr>
2017-02-08 17:24:04 +0100

    ADD : bootstrap react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Grid, Navbar, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	  	<Navbar inverse>
+          <Grid>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="/">React Project Charge Calculator</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+          </Grid>
+	  	</Navbar>
+		<Grid bsClass="container-fluid">
+			<Row>
+				<Col xs={10} xsOffset={1}>
+				</Col>
+			</Row>
+		</Grid>
       </div>
     );
   }
